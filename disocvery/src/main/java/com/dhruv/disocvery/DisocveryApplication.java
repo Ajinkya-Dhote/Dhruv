c2b91f5b20b24a821b701db8b6b815ff8579ba78
Ajinkya <ajinkyad@winsoftech.com>
2020-06-02 17:28:02 +0530

    initial server commit

---
+++ disocvery/src/main/java/com/dhruv/disocvery/DisocveryApplication.java
@@ -0,0 +1,13 @@
+package com.dhruv.disocvery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisocveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisocveryApplication.class, args);
+	}
+
+}

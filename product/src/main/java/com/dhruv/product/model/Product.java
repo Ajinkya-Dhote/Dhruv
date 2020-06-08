3fa9ffb52b7c2a935e05e3d791da9071aadc4bac
Ajinkya Dhote <11018034+Ajinkya-Dhote@users.noreply.github.com>
2020-06-08 14:32:22 +0530

    Added support for JPA Repository (#7)
    Co-authored-by: Ajinkya <ajinkyad@winsoftech.com>

--- product/src/main/java/com/dhruv/product/model/Product.java
+++ product/src/main/java/com/dhruv/product/model/Product.java
@@ -1,21 +1,27 @@
 package com.dhruv.product.model;
 
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 
-import javax.persistence.Column;
+import javax.persistence.*;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Entity
+@Table(name = "products")
 public class Product {
     @NonNull
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
     String name;
     Double price;
 
     @Column(name="date_first_available")
+    @CreatedDate
     Date dateFirstAvailable;
 
     boolean available;

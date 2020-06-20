1c44434c874913f4dfab34313814a0dd577371dd
Ajinkya Dhote <11018034+Ajinkya-Dhote@users.noreply.github.com>
2020-06-20 20:54:33 +0530

    Added swagger support (#10)
    Co-authored-by: Ajinkya <ajinkyad@winsoftech.com>

--- product/src/main/java/com/dhruv/product/ProductApplication.java
+++ product/src/main/java/com/dhruv/product/ProductApplication.java
@@ -1,11 +1,14 @@
 package com.dhruv.product;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@OpenAPIDefinition(info = @Info(title = "Product APi", description = "API tp access product capapbilities"))
 public class ProductApplication {
 
 	public static void main(String[] args) {

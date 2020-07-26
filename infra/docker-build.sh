de6a2a10e8cef4cb99e2b8e4e5ec0b0c1a6011ed
Ajinkya <ajinkya.c.dhote@gmail.com>
2020-07-26 20:14:32 +0530

    ui

--- infra/docker-build.sh
+++ infra/docker-build.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-for project in disocvery gateway product
+for project in disocvery gateway product ui
 do
         docker build --tag ajinkyadhote/dhruv-$project ../$project/ && docker push ajinkyadhote/dhruv-$project &
 done

d497b346ea28a36fb88a8ca11db9788dee490777
Ajinkya Dhote <11018034+Ajinkya-Dhote@users.noreply.github.com>
2020-07-26 20:15:56 +0530

    ui (#28)
    Co-authored-by: Ajinkya <ajinkya.c.dhote@gmail.com>

--- ui/src/app/components/home/home.component.ts
+++ ui/src/app/components/home/home.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 import { ResourceService } from '@services/resource.service';
 // import { GreetingComponent } from '@reusable-components/greeting/greeting.component';
+interface City {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -11,10 +15,23 @@ import { ResourceService } from '@services/resource.service';
 export class HomeComponent implements OnInit {
 
   products: any;
+  selectedCity: any;
+
+  cities: City[] = [
+    {value: 'nagpur', viewValue: 'Nagpur'},
+    {value: 'mumbai', viewValue: 'Mumbai'},
+    {value: 'pune', viewValue: 'Pune'}
+  ];
+
+  baseProducts: any[] = ["Wheat", "Besan", "Jowar", "Bajari"];
+
+
 
   constructor(private resource: ResourceService) { }
 
   ngOnInit(): void {
+      this.selectedCity = "nagpur";
+
     this.resource.getProductList()
         .subscribe(products => {
           console.log(products);
